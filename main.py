9532683bbec6955d32a5d7e055ce6a55291380ca
Evgeniya Speranskaya <ye.speranskaya@mail.ru>
2024-05-18 16:48:49 +0500

    сделала запись в файл

--- main.py
+++ main.py
@@ -20,43 +20,123 @@
 # https://skillbox.ru/media/code/parsing-sayta-vmeste-s-python-i-bibliotekoy-beautiful-soup-prostaya-instruktsiya-v-tri-shaga/
 
 # импортируем модуль
+# import requests
+# from bs4 import BeautifulSoup
+#
+# st_accept = "text/html" # говорим веб-серверу,
+#                         # что хотим получить html
+# # имитируем подключение через браузер Mozilla на macOS
+# st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
+# # формируем хеш заголовков
+# headers = {
+#    "Accept": st_accept,
+#    "User-Agent": st_useragent
+# }
+#
+# t = True
+# a = 0
+# while (t == True):
+#    # отправляем запрос с заголовками по нужному адресу
+#    req = requests.get("https://edition.cnn.com/", headers)
+#    # считываем текст HTML-документа
+#    src = req.text
+#    #print(src)
+#
+#
+#    # инициализируем html-код страницы
+#    soup = BeautifulSoup(src, 'lxml')
+#    # считываем заголовок страницы
+#    title = soup.title.string
+#    if "void" in soup:
+#       print(title)
+#    a += 1
+#    if a == 25:
+#       t = False
+#    # else:
+#    #    print("не нашел")
+#    # Программа выведет: Курсы - Блог компании Селектел
+#
+#    # надо устанавливать определнное время запуска
+#    # нудо устанавливать поисковик
+#    # надо установить запись в файл
+
+
 import requests
 from bs4 import BeautifulSoup
 
-st_accept = "text/html" # говорим веб-серверу,
-                        # что хотим получить html
-# имитируем подключение через браузер Mozilla на macOS
-st_useragent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 12_3_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.4 Safari/605.1.15"
-# формируем хеш заголовков
-headers = {
-   "Accept": st_accept,
-   "User-Agent": st_useragent
-}
 
-t = True
-a = 0
-while (t == True):
-   # отправляем запрос с заголовками по нужному адресу
-   req = requests.get("https://edition.cnn.com/", headers)
-   # считываем текст HTML-документа
-   src = req.text
-   #print(src)
+def get_news():
+    # URL веб-страницы новостного агентства
+    url = 'https://ngzt.ru/novosti-rossii/'
+
+    # Отправка запроса к веб-странице
+    response = requests.get(url)
+
+    # Анализ HTML-кода страницы
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    # Извлечение заголовков статей
+    headlines = soup.find_all('h4')
+    print(headlines)
+
+    # Извлечение аннотаций статей
+    annotations = soup.find_all('p')
+
+    # Извлечение авторов статей
+    authors = soup.find_all('a')
+
+# Фильтрация статей по содержанию
+    filtered_articles = []
+    for headline, annotation, author in zip(headlines, annotations, authors):
+        #if 'republican party' in headline.text or 'democratic party' in headline.text:
+        if 'село' in headline.text:
+            filtered_articles.append({
+                'title': headline.text,
+                'annotation': annotation.text,
+                'author': author.text
+            })
+    return filtered_articles
+
+# проверка наличия текста в лог-файле
+# def text_in_file(text, file):
+#     if text in file:
+#         return True
+#     else:
+#         False
+
+# функция, которая записывает данные с сайта
+def log_in_file(text):
+    # открываем файл для проверки
+    with open('example.txt', 'r') as file:
+        content = file.read()
+        # превращаю list в строку
+        str_text = ', '.join(text)
+    if str_text in content:
+        file.close()
+    else:
+    # Открываем файл для записи
+        with open('example.txt', 'a') as file:
+        # Записываем текст в файл
+            file.write(str_text + '\n')
+        # Закрываем файл
+            file.close()
+
+res = True
+count = 0
+
+# Создаем новый файл
+with open('example.txt', 'w') as file:
+    file.close()
+
+while (res == True):
+    log_in_file(get_news())
+    count += 1
+    if count == 25:
+        res = False
 
 
-   # инициализируем html-код страницы
-   soup = BeautifulSoup(src, 'lxml')
-   # считываем заголовок страницы
-   title = soup.title.string
-   if "void" in soup:
-      print(title)
-   a += 1
-   if a == 25:
-      t = False
-   # else:
-   #    print("не нашел")
-   # Программа выведет: Курсы - Блог компании Селектел
 
-   # надо устанавливать определнное время запуска
-   # нудо устанавливать поисковик
-   # надо установить запись в файл
+
+# if __name__ == 'USA':
+#     print(get_news())
 
