288b19d26726f5ab7a1b2a8107758835cad78a2a
Evgeniya Speranskaya <ye.speranskaya@mail.ru>
2024-05-29 19:02:42 +0500

    создала переменную имени файла

--- main.py
+++ main.py
@@ -6,9 +6,34 @@
 выводить нужно новости содержащие упоминания Республиканской и Демократической партий США.
 '''
 class File_log():
+    # переменная класса File_log обозначающая имя файла
+    name_file = ""
 
+    # функция создающая лог-файл (открывает и закрывает его)
     def __init__(self, name_file):
-        self.new_file = open(name_file, "x")
-        self.new_file.close()
+        self.name_file = name_file
+        function_create = open(name_file, "x")
+        function_create.close()
 
-my_file = File_log.__init__(File_log, "test.txt")
+    '''
+    функция возвращающая название имени файла
+    '''
+    def get_name_file(self):
+        return self.name_file
+
+
+
+    def write_text_in_file(self, text):
+        self.name_file = open(self, "a")
+        self.name_file.write(text + '\n')
+        self.name_file.close()
+        return self.name_file
+
+
+
+def start():
+    name_file = "CNN"
+    my_file = File_log.__init__(File_log, "CNN.txt")
+    File_log.write_text_in_file(my_file, "rsdrfsfsefsefsef")
+
+start()
